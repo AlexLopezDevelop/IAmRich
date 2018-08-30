084b3cdbc0f1b79bbe45485ea668a3e755d9196a
Cristian Lopez <alex_lopez.es@icloud.com>
2018-08-30 20:32:11 +0200

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Alex Lopez on 30/08/2018.
+//  Copyright © 2018 Alex Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
